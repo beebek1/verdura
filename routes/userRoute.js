3fbc73c5fd99b918eb4d0e68dfde72782beff4a1
beebek1 <beebek990@gmail.com>
2025-12-30 23:47:40 +0545

    modified folder structure

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -2,24 +2,22 @@ const router = require('express').Router();
 
 const {registerUser, userLogin, deleteUser, forgotPassword} = require('../controllers/authController');
 const {verifyEmail} = require('../helpers/verifyEmail');
-
-const authMiddleware = require("../helpers/authMiddleware");
-const roleMiddleware = require("../helpers/roleMiddleware");
-const {campaignPost, blogPost} = require("../controllers/orgController");
 const resetPassword = require("../helpers/resetPassword");
 
+//for all users
 
-//for all user
+//post request
 router.post('/register', registerUser);
 router.post('/login', userLogin);
-router.get('/verify-email', verifyEmail);
-router.delete('/delete', authMiddleware, deleteUser);
 router.post('/forgot-password', forgotPassword);
 router.post('/reset-password', resetPassword);
 
-//for orgs
-router.post('/blogpost',authMiddleware,roleMiddleware("org"), blogPost);
-router.post('/campaignpost',authMiddleware,roleMiddleware("org"), campaignPost);
+//get request
+router.get('/verify-email', verifyEmail);
+router.get("/")
+
+//delete request
+router.delete('/delete', authMiddleware, deleteUser);
 
 
 module.exports = router;
