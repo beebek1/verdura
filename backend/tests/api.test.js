49da08acfa7b0ee5904aa7ca2e6613c5afde58aa
Anuskaneupane69 <69210anuskarbs@gmail.com>
2026-01-28 23:24:09 +0545

    test for login user passed

--- backend/tests/api.test.js
+++ backend/tests/api.test.js
@@ -1,28 +1,8 @@
 const request = require ("supertest")
 require("dotenv").config();
 
-const BASE_URL = `http://localhost:${process.env.PORT || 5000}`;
+const BASE_URL = `http://localhost:${process.env.PORT || 3000}`;
 
-describe(`open API Tests`, ()=>{
-    it(`should create a new user`, async ()=>{
-        const uniqueUsername = `testuser${Date.now()}`;
-        const uniqueEmail = `unique${Date.now()}@gmail.com`;
-
-        const res = await request(BASE_URL)
-        .post(`/api/user/register`)
-        .send({
-            username : uniqueUsername,
-            email : uniqueEmail,
-            password : 'securepassword123'
-        });
-
-        expect(res.body.success).toBe(true);
-        expect(res.body.message).toBe('User added successfully');
-        expect(res.body.user.email).toBe(uniqueEmail);
-    });
-    
-    
-})
 describe(`register user`, ()=>{
     it(`should create a new user`, async ()=>{
         const uniqueUsername = `testuser${Date.now()}`;
@@ -40,5 +20,19 @@ describe(`register user`, ()=>{
         expect(res.body.success).toBe(true);
         expect(res.body.message).toBe('User added successfully');
         expect(res.body.user.email).toBe(uniqueEmail);
+    })
+})
+
+describe(`login user`, ()=>{
+    it(`should login a user`, async()=>{
+        const res = await request(BASE_URL)
+        .post(`/api/auth/login`)
+        .send({
+            email : 'abc@mailinator.com',
+            password : 'Pa$$w0rd!'
+        });
+
+        expect(res.body.success).toBe(true);
+        expect(res.body.message).toBe('login successful')
     })
 })
